9327ffe731664f312cbb26ad4ea9ba8620128240
sponsorCoinAdmin <sponsorcoin@protonmail.com>
2022-11-29 03:20:44 -0500

    Add SPCOINABI

--- src/AlphaRouterService.js
+++ src/AlphaRouterService.js
@@ -3,7 +3,7 @@ const { Token, CurrencyAmount, TradeType, Percent } = require('@uniswap/sdk-core
 const { ethers, BigNumber } = require('ethers')
 const JSBI = require('jsbi')
 const ERC20ABI = require('./interfaces/abi.json')
-const SPCOINABI = require('./interfaces/abi.json')
+const SPCOINABI = require('./interfaces/spCoin.json')
 
 const V3_SWAP_ROUTER_ADDRESS = '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45'
 const REACT_APP_INFURA_URL_TESTNET = process.env.REACT_APP_INFURA_URL_TESTNET
